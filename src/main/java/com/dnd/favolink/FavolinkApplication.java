e7264c2d3794a7e7691237e69f1a25653e96a393
Jimin <kaley0421@naver.com>
2024-02-07 19:33:43 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/dnd/favolink/FavolinkApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.favolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavolinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavolinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/favolink/FavolinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.favolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavolinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
