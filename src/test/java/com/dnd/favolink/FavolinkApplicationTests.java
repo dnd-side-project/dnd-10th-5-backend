61d99c36cde85bae56c86d41d35721ef5c281543
MingyeomKim <mingyum119@naver.com>
2024-02-09 00:52:49 +0900

    fix: @SpringBootTest 를 제거하여 테스트 실패 해결

--- src/test/java/com/dnd/favolink/FavolinkApplicationTests.java
+++ src/test/java/com/dnd/favolink/FavolinkApplicationTests.java
@@ -3,7 +3,7 @@ package com.dnd.favolink;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class FavolinkApplicationTests {
 
 	@Test
