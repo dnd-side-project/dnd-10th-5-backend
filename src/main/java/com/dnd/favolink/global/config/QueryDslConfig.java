0dae3a16a295b6e6c48ae05b9892249361cc268d
Jimin <kaley0421@naver.com>
2024-02-10 22:40:50 +0900

    feat: queryDsl 추가 (#1)

---
+++ src/main/java/com/dnd/favolink/global/config/QueryDslConfig.java
@@ -0,0 +1,18 @@
+package com.dnd.favolink.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
