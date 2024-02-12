9f6405d7a08a17a170c90986b71b2228bb2ff6e0
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2024-02-12 22:22:14 +0900

    feat: health check API 생성

---
+++ src/main/java/com/dnd/favolink/global/HealthController.java
@@ -0,0 +1,12 @@
+package com.dnd.favolink.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+}
