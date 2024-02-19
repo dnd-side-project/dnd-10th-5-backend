105433f006d6144c5c5d0f9678dacc3809467f1a
Jimin <kaley0421@naver.com>
2024-02-19 19:38:55 +0900

    feat: SecurityConfig 에 auth url 허용 (#3)

--- src/main/java/com/dnd/favolink/global/config/SecurityConfig.java
+++ src/main/java/com/dnd/favolink/global/config/SecurityConfig.java
@@ -21,7 +21,10 @@ public class SecurityConfig {
             "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources", "/swagger-resources/**",
 
             /* health check */
-            "/health"
+            "/health",
+
+            /* apis */
+            "/auth/**"
     };
 
     private final JwtTokenProvider jwtTokenProvider;
