8f906a2c42d0d4c5fcc40661bb356aa6c2dd1f01
Jimin <kaley0421@naver.com>
2024-03-11 16:52:09 +0900

    docs: bookmark 관련 API swagger 문서화 (#9)

--- src/main/java/com/dnd/favolink/bookmark/controller/BookmarkController.java
+++ src/main/java/com/dnd/favolink/bookmark/controller/BookmarkController.java
@@ -1,5 +1,9 @@
 package com.dnd.favolink.bookmark.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,19 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "💟 Bookmark")
 @RestController
 @RequestMapping("/bookmark")
 public class BookmarkController {
+    @Operation(summary = "💟 즐겨찾기 조회", description = "유저가 즐겨찾기 해둔 링크들을 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "유저가 즐겨찾기 해둔 링크들의 id 목록")
+    })
     @GetMapping
     public BookmarksResponse getBookmarks() {
         return null;
     }
 
+    @Operation(summary = "💟 즐겨찾기 추가", description = "특정 링크를 즐겨찾기에 추가합니다.")
     @PostMapping
     public void addBookmark(@RequestParam int linkId) {
 
     }
 
+    @Operation(summary = "💟 즐겨찾기 삭제", description = "특정 링크를 즐겨찾기에서 삭제합니다.")
     @DeleteMapping
     public void deleteBookmark(@RequestParam int linkId) {
 
